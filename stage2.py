809bc3972504c3a2c98bd48b77fdd3a5991d66ba
daniel.kurushin@gmail.com <daniel.kurushin@gmail.com>
2019-10-19 13:46:58 +0500

    stage 2

---
+++ stage2.py
@@ -0,0 +1,17 @@
+from utilites import load, dump
+
+_in = load('секторная структура.json')
+
+rez = {}
+for reg in _in.keys():
+    data = {}
+    for _pok in _in[reg].keys():
+        A = _in[reg][_pok]['ВРП']
+        B = _in[reg][_pok]['ЧИСЛ']
+        X = A / (B * 1000)
+        
+        data.update({_pok:X})
+
+    rez.update({reg:data})
+
+dump(rez, 'Производительность труда.json')
