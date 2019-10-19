b8742727e4b57e4b7675f5d06ef41d38e6c116d0
daniel.kurushin@gmail.com <daniel.kurushin@gmail.com>
2019-10-19 13:36:27 +0500

    секторная структура

---
+++ parser_b.py
@@ -0,0 +1,25 @@
+from xlrd import open_workbook,cellname
+
+wb = open_workbook('data.xlsx')
+
+ws = wb.sheet_by_name('Расчет производительности')
+
+rez = {}
+for row in range(4,88):
+    reg = ws.cell(row, 0).value.replace('\n', ' ').strip()
+    data = {}
+    for col in range(1, 14):
+        key1 = ws.cell(2, col).value.replace('\n', ' ').strip()
+        v1 = ws.cell(row, col).value
+        v2 = ws.cell(row, col+14).value
+        data.update({key1:{'ЧИСЛ':v1, 'ВРП':v2}})
+    rez.update({reg:data})
+
+dump(rez,'секторная структура.json')
+
+#        cat = ws.cell(0, col).value.replace('\n', ' ').strip()
+#        v = ws.cell(row, col).value
+#        data.update({cat:v})
+#    rez.update({reg:data})
+
+#
