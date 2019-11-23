630aea213b8836767fe2d4fed367900aa742ada1
mariarusinova <56752999+mariarusinova@users.noreply.github.com>
2019-11-23 19:11:06 +0500

    Добавлены юнит-тесты

--- stage3.py
+++ stage3.py
@@ -1,13 +1,16 @@
 from utilites import load, dump
 
-_in1 = load('jsondb/Количество людей с высшим образованием из числа занятых и безработных.json')
-_in2 = load('jsondb/исходные данные.json')
-_in3 = load('jsondb/Производительность труда.json')
+def stage_3(_in1, _in2, _in3):
+    for reg in _in2.keys():
+        x = _in1[reg]
+        _in2[reg]["Количество людей с высшим образованием из числа занятых и безработных (тыс.человек)"] = x
+        for k in _in3[reg].keys():
+            _in2[reg][k] = _in3[reg][k]
+    return _in2
 
-for reg in _in2.keys():
-    x = _in1[reg]
-    _in2[reg]["Количество людей с высшим образованием из числа занятых и безработных (тыс.человек)"] = x
-    for k in _in3[reg].keys():
-        _in2[reg][k] = _in3[reg][k]
-        
-dump(_in2, 'jsondb/исходные данные (ред).json')
+if __name__ == '__main__':
+    _in1 = load('jsondb/Количество людей с высшим образованием из числа занятых и безработных.json')
+    _in2 = load('jsondb/исходные данные.json')
+    _in3 = load('jsondb/Производительность труда.json')
+    _out = stage_3(_in1, _in2, _in3)  
+    dump(_out, 'jsondb/исходные данные (ред).json')
