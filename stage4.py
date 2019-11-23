37e5b2d45e18fe926c3617c0f1fbed405b17fc92
mariarusinova <56752999+mariarusinova@users.noreply.github.com>
2019-11-23 19:43:41 +0500

    Добавлены юнит-тесты

--- stage4.py
+++ stage4.py
@@ -1,26 +1,29 @@
 from utilites import load, dump
 
-_categories = load('jsondb/categories.json')
+def stage_4(_categories):
+    критерии = set([ _categories[k][1] for k in _categories.keys() ])
 
-критерии = set([ _categories[k][1] for k in _categories.keys() ])
+    число_критериев = len(критерии)
 
-число_критериев = len(критерии)
+    rez = {}
+    for критерий in критерии:
+        rez.update({критерий:0})
 
-rez = {}
-for критерий in критерии:
-    rez.update({критерий:0})
+    for показатель in _categories.keys():
+        критерий_показателя = _categories[показатель][1]
+        фактор_показателя = _categories[показатель][0]
 
-for показатель in _categories.keys():
-    критерий_показателя = _categories[показатель][1]
-    фактор_показателя = _categories[показатель][0]
-    
-    rez[критерий_показателя] += 1
+        rez[критерий_показателя] += 1
 
-M = {}
-for показатель in _categories.keys():
-    критерий_показателя = _categories[показатель][1]
-    число_показателей = rez[критерий_показателя]
-    m = 1/число_критериев/число_показателей
-    M.update({показатель:m})
+    M = {}
+    for показатель in _categories.keys():
+        критерий_показателя = _categories[показатель][1]
+        число_показателей = rez[критерий_показателя]
+        m = 1/число_критериев/число_показателей
+        M.update({показатель:m})
+    return M
 
-dump(M,'jsondb/вес показателей.json')
+if __name__ == '__main__':
+    _categories = load('jsondb/categories.json')
+    stage_4(_categories)
+    dump(M,'jsondb/вес показателей.json')
